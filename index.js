31de7306f78ceffb1b7dd4bcc95ab2c97e8ae4d3
Ivshti <ivo@linvo.org>
2015-12-23 16:43:38 +0200

    Change some things on the dataset

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ var manifest = {
 };
 
 var dataset = {
-    "tt0063350": { infoHash: "f17fb68ce756227fce325d0513157915f5634985", mapIdx: 0, availability: 2 }, // night of the living dead, 1968
+    // For p2p streams, you can provide availability property, from 0 to 3, to indicate stability of the stream; if not passed, 1 will be assumed
+    // mapIdx is the index of the file within the torrent ; if not passed, the largest file will be selected
     "tt0032138": { infoHash: "24c8802e2624e17d46cd555f364debd949f2c81e", mapIdx: 0, availability: 2 }, // the wizard of oz 1939
     "tt0017136": { infoHash: "dca926c0328bb54d209d82dc8a2f391617b47d7a", mapIdx: 1, availability: 2 }, // metropolis, 1927
 
-    // house on haunted hill 1959
-    // if mapIdx is not passed, largest file is picked ; if availability is not passed, 1 is assumed
-    "tt0051744": { infoHash: "9f86563ce2ed86bbfedd5d3e9f4e55aedd660960" },
-    
+    "tt0063350": { infoHash: "f17fb68ce756227fce325d0513157915f5634985" }, // night of the living dead, 1968
+    "tt0051744": { infoHash: "9f86563ce2ed86bbfedd5d3e9f4e55aedd660960" }, // house on haunted hill 1959
+
     "tt1254207": { url: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4", availability: 1 }, // big buck bunny, HTTP stream
     "tt0031051": { yt_id: "gLKA7wxqtfM", availability: 2 } // The Arizona Kid, 1939; YouTube stream
 };
