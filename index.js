c46e45e6e71fa3640be858b847ed00de14fa4ee4
Alexandru Branza <branza.alex@gmail.com>
2018-09-18 13:02:56 +0300

    Set Item Type in Dataset

--- index.js
+++ index.js
@@ -41,23 +41,23 @@ var manifest = {
 
 var dataset = {
     // fileIdx is the index of the file within the torrent ; if not passed, the largest file will be selected
-    "tt0032138": { name: "The Wizard of Oz", infoHash: "24c8802e2624e17d46cd555f364debd949f2c81e", fileIdx: 0 },
-    "tt0017136": { name: "Metropolis", infoHash: "dca926c0328bb54d209d82dc8a2f391617b47d7a", fileIdx: 1 },
+    "tt0032138": { name: "The Wizard of Oz", type: "movie", infoHash: "24c8802e2624e17d46cd555f364debd949f2c81e", fileIdx: 0 },
+    "tt0017136": { name: "Metropolis", type: "movie", infoHash: "dca926c0328bb54d209d82dc8a2f391617b47d7a", fileIdx: 1 },
 
     // night of the living dead, example from magnet
-    "tt0063350": fromMagnet("Night of the Living Dead", "magnet:?xt=urn:btih:A7CFBB7840A8B67FD735AC73A373302D14A7CDC9&dn=night+of+the+living+dead+1968+remastered+bdrip+1080p+ita+eng+x265+nahom&tr=udp%3A%2F%2Ftracker.publicbt.com%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce"),
-    "tt0051744": { name: "House on Haunted Hill", infoHash: "9f86563ce2ed86bbfedd5d3e9f4e55aedd660960" },
+    "tt0063350": fromMagnet("Night of the Living Dead", "movie", "magnet:?xt=urn:btih:A7CFBB7840A8B67FD735AC73A373302D14A7CDC9&dn=night+of+the+living+dead+1968+remastered+bdrip+1080p+ita+eng+x265+nahom&tr=udp%3A%2F%2Ftracker.publicbt.com%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce"),
+    "tt0051744": { name: "House on Haunted Hill", type: "movie", infoHash: "9f86563ce2ed86bbfedd5d3e9f4e55aedd660960" },
 
-    "tt1254207": { name: "Big Buck Bunny", url: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" }, // HTTP stream
-    "tt0031051": { name: "The Arizona Kid", ytId: "m3BKVSpP80s" }, // YouTube stream
+    "tt1254207": { name: "Big Buck Bunny", type: "movie", url: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" }, // HTTP stream
+    "tt0031051": { name: "The Arizona Kid", type: "movie", ytId: "m3BKVSpP80s" }, // YouTube stream
 
-    "tt0137523": { name: "Fight Club", externalUrl: "https://www.netflix.com/watch/26004747" }, // redirects to Netflix
+    "tt0137523": { name: "Fight Club", type: "movie", externalUrl: "https://www.netflix.com/watch/26004747" }, // redirects to Netflix
 
-    "tt1748166:1:1": { name: "Pioneer One", infoHash: "07a9de9750158471c3302e4e95edb1107f980fa6" }, // torrent for season 1, episode 1
+    "tt1748166:1:1": { name: "Pioneer One", type: "series", infoHash: "07a9de9750158471c3302e4e95edb1107f980fa6" }, // torrent for season 1, episode 1
 };
 
 // utility function to add from magnet
-function fromMagnet(name, uri) {
+function fromMagnet(name, type, uri) {
     var parsed = magnet.decode(uri);
     var infoHash = parsed.infoHash.toLowerCase();
     var tags = [];
@@ -65,6 +65,7 @@ function fromMagnet(name, uri) {
     if (uri.match(/1080p/i)) tags.push("1080p");
     return {
         name: name,
+        type: type,
         infoHash: infoHash,
         sources: (parsed.announce || []).map(function(x) { return "tracker:"+x }).concat(["dht:"+infoHash]),
         tag: tags,
@@ -87,13 +88,6 @@ addon.defineStreamHandler(function(args, cb) {
 
 })
 
-// determine media type by index
-var typeFromId = function(index) {
-    // object keys that include ":" are series, for example:
-    // "tt1748166:1:1" refers to season 1, episode 1 of the "tt1748166" imdb id
-    return index.includes(':') ? 'series' : 'movie'
-}
-
 var METAHUB_URL = 'https://images.metahub.space'
 
 var basicMeta = function(data, index) {
@@ -102,7 +96,7 @@ var basicMeta = function(data, index) {
     var imdbId = index.split(':')[0]
     return {
         id: imdbId,
-        type: typeFromId(index),
+        type: data.type,
         name: data.name,
         poster: METAHUB_URL+'/poster/medium/'+imdbId+'/img',
     }
@@ -115,7 +109,7 @@ addon.defineCatalogHandler(function(args, cb) {
     // iterate dataset object and only add movies or series
     // depending on the requested type
     for (var key in dataset) {
-        if (args.type == typeFromId(key)) {
+        if (args.type == dataset[key].type) {
             metas.push(basicMeta(dataset[key], key))
         }
     }
