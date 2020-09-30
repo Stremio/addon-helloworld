bc1ae9e5ae745c163741b0c240585953a99614ef
Shteryana Shopova <s.shopova@stremio.com>
2020-09-30 14:45:09 +0300

    Allow specifying custom listen port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const { serveHTTP } = require("stremio-addon-sdk");
 
 const addonInterface = require("./addon");
-serveHTTP(addonInterface, { port: 7000 });
+serveHTTP(addonInterface, { port: process.env.PORT || 7000 });
 
